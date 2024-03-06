0c757ccb7b35d6a036bcf445e6bbd0c90bc2346b
Yamil Castillo <yamilcs@gmail.com>
2024-03-05 23:54:41 -0300

    Generando middleware authToken

--- src/api/v1/controllers/usersController.js
+++ src/api/v1/controllers/usersController.js
@@ -56,8 +56,7 @@ const getUser = async (req, res) => {
     const user = await findUserbyEmail(email);
     res.status(200).json([user]);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
+        res.status(500).json({ error: error.message });
   }
 };
 export { createNewUser, loginUser, getUser };
